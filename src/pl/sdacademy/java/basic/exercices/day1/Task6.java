749cf48c5c29b3202d0a2f20d316866c1480bcf9
Marcin Stus <marcinstus@gmail.com>
2023-12-09 15:50:17 +0100

    Task6 solution

---
+++ src/pl/sdacademy/java/basic/exercices/day1/Task6.java
@@ -0,0 +1,26 @@
+package pl.sdacademy.java.basic.exercices.day1;
+
+public class Task6 {
+
+    public static void main(String[] args) {
+        int zakresPoczątek = 0;
+        int zakresKoniec = 1000000;
+        for (int i = zakresPoczątek; i <= zakresKoniec; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static boolean isPrimeNumber(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
