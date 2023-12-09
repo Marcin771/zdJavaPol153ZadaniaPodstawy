ceb74bf53f1347213f27538e88130e6a9cac1737
Marcin Stus <marcinstus@gmail.com>
2023-12-09 11:20:40 +0100

    Task1

---
+++ src/pl/sdacademy/java/basic/exercices/day1/Task1.java
@@ -0,0 +1,55 @@
+package pl.sdacademy.java.basic.exercices.day1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        displayWordUsingDecimal();
+
+        System.out.println();
+
+        displayWordUsingHexa();
+
+        System.out.println();
+
+        displayWordUsingBinary();
+
+    }
+
+    private static void displayWordUsingBinary() {
+        // binary
+        // 0b przed liczbą interpretuje to jako binary
+        //01010011  01000100 01000001
+        char s2 = 0b01010011;
+        char d2 = 0b01000100;
+        char a2 = 0b01000001;
+
+        System.out.print(s2);
+        System.out.print(d2);
+        System.out.print(a2);
+    }
+
+    private static void displayWordUsingHexa() {
+        // hex 16stkowy zapis
+        // 0x traktuje to jako system szesnastkowy
+        //53 44 41
+        char s1 = 0x53;
+        char d1 = 0x44;
+        char a1 = 0x41;
+
+        System.out.print(s1);
+        System.out.print(d1);
+        System.out.print(a1);
+    }
+
+    private static void displayWordUsingDecimal() {
+        // decimal
+        char s = 83;
+        char d = 68;
+        char a = 65;
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
+    }
+
+}
