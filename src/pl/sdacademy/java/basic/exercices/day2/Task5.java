81d4f7984c7f2f6918ad377315a9d0b44cc19048
Marcin Stus <marcinstus@gmail.com>
2023-12-10 12:47:15 +0100

    Task7 solution and Task5 upgrade

--- src/pl/sdacademy/java/basic/exercices/day2/Task5.java
+++ src/pl/sdacademy/java/basic/exercices/day2/Task5.java
@@ -8,10 +8,11 @@ public class Task5 {
         System.out.println(numberOfExistingCharacter);
     }
 
-    private static int occurrenceNumber(String input, char char1) {
+    private static int occurrenceNumber(String input, char searchedCharacter) {
         int counter = 0;
-        String stringChar = String.valueOf(char1);
-        if (StringValidator.isValid(input) && StringValidator.isValid(stringChar)) {
+        String stringChar = String.valueOf(searchedCharacter);
+        if (StringValidator.isValid(input) && StringValidator.isValid(stringChar) && input.contains(String.valueOf(searchedCharacter))) {
+            //int k=input.indexOf(searchedCharacter); pętla może być wydajniejsza
             for (int i = 0; i < input.length() ; i++) {
                 if (input.substring(i, i + 1).equals(stringChar)) {
                     counter++;
