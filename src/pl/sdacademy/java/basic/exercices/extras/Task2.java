e59781ab79678a20d80424e04fa2019f6b79b32d
Marcin Stus <marcinstus@gmail.com>
2023-12-15 21:49:32 +0100

    Task2 solution

---
+++ src/pl/sdacademy/java/basic/exercices/extras/Task2.java
@@ -0,0 +1,16 @@
+package pl.sdacademy.java.basic.exercices.extras;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("First char:");
+        String firstChar = scanner.nextLine();
+        System.out.print("First char:");
+        String secondChar = scanner.nextLine();
+        int distanceBetwenChars = Math.abs(firstChar.charAt(0) - secondChar.charAt(0));
+        System.out.println("Distance: " + distanceBetwenChars);
+    }
+}
