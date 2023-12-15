f9e36e0c12f86df934e84bfc02d135b9ab4b8488
Marcin Stus <marcinstus@gmail.com>
2023-12-15 19:55:22 +0100

    Task6 fix

--- src/pl/sdacademy/java/basic/exercices/day2/Task6.java
+++ src/pl/sdacademy/java/basic/exercices/day2/Task6.java
@@ -6,13 +6,21 @@ public class Task6 {
         String input = "Ala ma kota";
         char char1 = 'a';
         int numberOfExistingCharacter = occurrenceNumber(input, char1);
-        System.out.println(numberOfExistingCharacter);
+        System.out.println("Input: " + input);
+        System.out.println("Serched Character: " + char1);
+        System.out.println("Result: " + numberOfExistingCharacter);
+
+        char1 = 'z';
+        numberOfExistingCharacter = occurrenceNumber(input, char1);
+        System.out.println("Input: " + input);
+        System.out.println("Serched Character: " + char1);
+        System.out.println("Result: " + numberOfExistingCharacter);
     }
 
     private static int occurrenceNumber(String input, char char1) {
         int counter = 0;
         String stringChar = String.valueOf(char1);
-        if (StringValidator.isValid(input) && StringValidator.isValid(stringChar)) {
+        if (StringValidator.isValid(input) && StringValidator.isValid(stringChar) && input.contains(String.valueOf(char1))) {
             return input.indexOf(stringChar);
         }
         return -1;
