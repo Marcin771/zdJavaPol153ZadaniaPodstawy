432ee6298c280852ba49381a93154835c277665e
Marcin Stus <marcinstus@gmail.com>
2023-12-16 08:32:56 +0100

    Task7 solution

--- src/pl/sdacademy/java/basic/exercices/extras/Task7.java
+++ src/pl/sdacademy/java/basic/exercices/extras/Task7.java
@@ -7,9 +7,9 @@ public class Task7 {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        //method need to be fixed
-        //int sumOfElementsOnDiagonal = sumNumbersOnDiagonals(array);
-        //System.out.println("Sum of elements on diagonals: " + sumOfElementsOnDiagonal);
+
+        int sumOfElementsOnDiagonal = sumNumbersOnDiagonals(array);
+        System.out.println("Sum of elements on diagonals: " + sumOfElementsOnDiagonal);
 
         int sumOfElementsOnEdges = sumNumbersOnEdges(array);
         System.out.println("Sum of elements on edges: " + sumOfElementsOnEdges);
@@ -30,20 +30,15 @@ public class Task7 {
     }
 
     private static int sumNumbersOnDiagonals(int[][] array) {
-        //method need to be fixed
         int sum = 0;
         int rows = array.length;
         int cols = array[0].length;
 
 
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (i == j || cols - 1 - i == j) {
-                    sum += array[i][j];
-                }
-            }
+            sum += array[i][i];
+            sum += array[i][cols - 1 - 1];
         }
-
         return sum;
     }
 }
