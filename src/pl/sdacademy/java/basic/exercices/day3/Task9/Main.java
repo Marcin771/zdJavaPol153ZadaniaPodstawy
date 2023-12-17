ee3303e1971b2c74b118963dfc7799d8797747ce
Marcin Stus <marcinstus@gmail.com>
2023-12-17 15:19:19 +0100

    Task9 solution

---
+++ src/pl/sdacademy/java/basic/exercices/day3/Task9/Main.java
@@ -0,0 +1,15 @@
+package pl.sdacademy.java.basic.exercices.day3.Task9;
+
+public class Main {
+    public static void main(String[] args) {
+        OrderItem cukier = new OrderItem("Cukier", 3, 4);
+        OrderItem mleko = new OrderItem("Mleko", 10, 3);
+
+        Order order = new Order(2);
+        order.addItem(cukier);
+        order.addItem(mleko);
+//        order.addItem(mleko);
+
+        order.print();
+    }
+}
